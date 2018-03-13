a42ab927ac9a94768c8a85fa04b402642fd9e884
J-A16 <Jeremy.Alexandr@gmail.com>
2018-03-12 21:20:14 -0400

    Add files via upload

--- HW6/NQueens.java
+++ HW6/NQueens.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 class NQueensSolver implements Runnable {
 	public Thread t;
@@ -12,14 +14,22 @@ class NQueensSolver implements Runnable {
 	public static ConcurrentHashMap<String, int[][]> solutions = new ConcurrentHashMap<String, int[][]>();
 	public static ConcurrentHashMap<String, int[][]> middleSolutions = new ConcurrentHashMap<String, int[][]>();
 	public static ConcurrentHashMap<Integer, String> assignments = new ConcurrentHashMap<Integer, String>();
+	public static AtomicLong totalUnitsDone = new AtomicLong();
+	public static AtomicBoolean canPrint = new AtomicBoolean(true);
+	public static long lastOutput;
 	private int numbersOfSquares;
 	private int n;
+	private long[] positionsOfQueens;
+	private long configurationStart;
 	private long configuration;
 	private long configurationStop;
-	private long[] positionsOfQueens;
+	private long totalNumUnits;
+	private long UNIT = 1000000000000L;
+	private long unitsDone;
 	private boolean middle;
 
-	NQueensSolver(int threadNum, int n, long configuration, long configurationStop, boolean middle) {
+	NQueensSolver(int threadNum, int n, long configuration, long configurationStop, long totalNumConfigurations,
+			boolean middle) {
 
 		this.threadName = "solver" + threadNum;
 
@@ -29,11 +39,19 @@ class NQueensSolver implements Runnable {
 
 		positionsOfQueens = new long[n];
 
+		this.configurationStart = configuration;
+
 		this.configuration = configuration;
 
 		this.configurationStop = configurationStop;
 
+		this.totalNumUnits = totalNumConfigurations / UNIT;
+
+		unitsDone = 0L;
+
 		this.middle = middle;
+
+		lastOutput = System.currentTimeMillis();
 
 		assignments.put(threadNum, configuration + " - " + configurationStop);
 	}
@@ -50,7 +68,44 @@ class NQueensSolver implements Runnable {
 
 				board = positionQueens(positionsOfQueens, n);
 
-				addSolution(board, solutions, middle);
+				addSolution(board, solutions, middleSolutions, middle);
+			}
+
+			if (configuration > configurationStop) {
+				configuration = configurationStop;
+			}
+
+			if (System.currentTimeMillis() - lastOutput > 300000) {
+				if (canPrint.getAndSet(false)) {
+
+					System.out.println(threadName + "  " + ((configuration - configurationStart) / UNIT) + " / "
+							+ ((configurationStop - configurationStart) / UNIT));
+
+					long currentTime = System.currentTimeMillis();
+					lastOutput = currentTime;
+
+					canPrint.set(true);
+				}
+			}
+
+			if (unitsDone < (configuration - configurationStart) / UNIT) {
+				if (canPrint.getAndSet(false)) {
+					long moreUnitsDone = ((configuration - configurationStart) / UNIT) - unitsDone;
+
+					for (long i = 0; i < moreUnitsDone; ++i) {
+						++unitsDone;
+						System.out.println(totalUnitsDone.addAndGet(1L) + " / " + totalNumUnits);
+						// System.out.println(threadName + " " + ((configuration
+						// - configurationStart) / UNIT) + " / "
+						// + ((configurationStop - configurationStart) / UNIT));
+
+						long currentTime = System.currentTimeMillis();
+						System.out.println((currentTime - lastOutput) / 1000.0 + " Seconds");
+						lastOutput = currentTime;
+					}
+
+					canPrint.set(true);
+				}
 			}
 
 			++configuration;
@@ -59,10 +114,10 @@ class NQueensSolver implements Runnable {
 	}
 
 	long getPositionsOfQueens(long[] positionsOfQueens, long configuration, int numSquares) {
-
 		boolean done = false;
 
 		while (!done) {
+
 			done = true;
 
 			positionsOfQueens[positionsOfQueens.length - 1] = configuration;
@@ -79,21 +134,21 @@ class NQueensSolver implements Runnable {
 			int row, column;
 
 			for (int i = 0; i < N; ++i) {
-
+				
 				row = (int) positionsOfQueens[i] / N;
 
 				if (!rows.contains(row)) {
 					rows.add(row);
 				} else {
 					column = (int) positionsOfQueens[i] % N;
-					configuration += (N - column) * Math.pow(N * N, N - (i + 1));
+					configuration += (N - column) * (long) Math.pow(N * N, N - (i + 1));
 					rows.add(row + 1);
 					done = false;
 				}
 			}
 
 			if (done) {
-
+				
 				HashSet<Integer> columns = new HashSet<Integer>();
 
 				for (int i = 0; i < N; ++i) {
@@ -102,7 +157,7 @@ class NQueensSolver implements Runnable {
 					if (!columns.contains(column)) {
 						columns.add(column);
 					} else {
-						configuration += Math.pow(N * N, N - (i + 1));
+						configuration += (long) Math.pow(N * N, N - (i + 1));
 						columns.add(column + 1);
 						done = false;
 					}
@@ -110,7 +165,7 @@ class NQueensSolver implements Runnable {
 			}
 
 			if (done) {
-
+				
 				HashSet<Integer> forwardDiagonals = new HashSet<Integer>();
 
 				int diagonal;
@@ -121,7 +176,7 @@ class NQueensSolver implements Runnable {
 					if (!forwardDiagonals.contains(diagonal)) {
 						forwardDiagonals.add(diagonal);
 					} else {
-						configuration += Math.pow(N * N, N - (i + 1));
+						configuration += (long) Math.pow(N * N, N - (i + 1));
 						forwardDiagonals.add(diagonal + 1);
 						done = false;
 					}
@@ -129,7 +184,7 @@ class NQueensSolver implements Runnable {
 			}
 
 			if (done) {
-
+				
 				HashSet<Integer> backwardDiagonals = new HashSet<Integer>();
 
 				int diagonal;
@@ -140,8 +195,8 @@ class NQueensSolver implements Runnable {
 					if (!backwardDiagonals.contains(diagonal)) {
 						backwardDiagonals.add(diagonal);
 					} else {
-						configuration += Math.pow(N * N, N - (i + 1));
-						backwardDiagonals.add(diagonal + 1);
+						configuration += (long) Math.pow(N * N, N - (i + 1));
+						backwardDiagonals.add(diagonal - 1);
 						done = false;
 					}
 				}
@@ -162,7 +217,7 @@ class NQueensSolver implements Runnable {
 		return board;
 	}
 
-	void addSolution(int[][] board, ConcurrentHashMap<String, int[][]> solutions, boolean middle) {
+	void addSolution(int[][] board, ConcurrentHashMap<String, int[][]> solutions, ConcurrentHashMap<String, int[][]> middleSolutions, boolean middle) {
 
 		if (!middle) {
 			String key = generateKey(board);
@@ -216,7 +271,7 @@ class NQueens {
 			// column
 			for (int i = 0; i < N; ++i) {
 
-				configuration += (i * N + i) * Math.pow(N * N, N - (i + 1));
+				configuration += (i * N + i) * (long) Math.pow(N * N, N - (i + 1));
 			}
 
 			// Once the Queen in the first position passes half way across the
@@ -229,16 +284,20 @@ class NQueens {
 
 			long configurationsPerThread = (configurationStop - configuration) / numberOfThreads;
 
+			System.out.println("Number per thread - " + configurationsPerThread);
+			System.out.println("Total configurations - " + (configurationStop - configuration));
+
 			NQueensSolver[] solvers = new NQueensSolver[numberOfThreads];
 
 			for (int i = 0; i < numberOfThreads; ++i) {
 				if (i != numberOfThreads - 1) {
 					solvers[i] = new NQueensSolver((i + 1), N, configuration + (configurationsPerThread * i),
-							configuration + (configurationsPerThread * (i + 1)), false);
+							configuration + (configurationsPerThread * (i + 1)), configurationStop - configuration,
+							false);
 					solvers[i].start();
 				} else {
 					solvers[i] = new NQueensSolver((i + 1), N, configuration + (configurationsPerThread * i),
-							configurationStop, false);
+							configurationStop, configurationStop - configuration, false);
 					solvers[i].start();
 				}
 			}
@@ -247,7 +306,6 @@ class NQueens {
 				try {
 					solvers[i].t.join();
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -260,9 +318,9 @@ class NQueens {
 			// column
 			for (int i = 0; i < N; ++i) {
 
-				configuration += (i * N + i) * Math.pow(N * N, N - (i + 1));
+				configuration += (i * N + i) * (long) Math.pow(N * N, N - (i + 1));
 			}
-			
+
 			// Once the Queen in the first position reaches the middle column,
 			// the other side of
 			// the middle column can be found by doubling the solutions found on
@@ -279,21 +337,26 @@ class NQueens {
 
 			int numberOfCenterThreads = numberOfThreads / 4;
 
-			long configurationsPerThread = (configurationStop - configuration) / numberOfLeftThreads;
+			long configurationsPerLeftThread = (configurationStop - configuration) / numberOfLeftThreads;
 
 			long configurationsPerCenterThread = (configurationMiddleStop - configurationMiddle)
 					/ numberOfCenterThreads;
+			
+			System.out.println("Number per left thread - " + configurationsPerLeftThread);
+			System.out.println("Number per center thread - " + configurationsPerCenterThread);
+			System.out.println("Total configurations - " + (configurationMiddleStop - configuration));
 
 			NQueensSolver[] solvers = new NQueensSolver[numberOfThreads];
 
 			for (int i = 0; i < numberOfLeftThreads; ++i) {
 				if (i != numberOfLeftThreads - 1) {
-					solvers[i] = new NQueensSolver((i + 1), N, configuration + (configurationsPerThread * i),
-							configuration + (configurationsPerThread * (i + 1)), false);
+					solvers[i] = new NQueensSolver((i + 1), N, configuration + (configurationsPerLeftThread * i),
+							configuration + (configurationsPerLeftThread * (i + 1)),
+							configurationMiddleStop - configuration, false);
 					solvers[i].start();
 				} else {
-					solvers[i] = new NQueensSolver((i + 1), N, configuration + (configurationsPerThread * i),
-							configurationStop, false);
+					solvers[i] = new NQueensSolver((i + 1), N, configuration + (configurationsPerLeftThread * i),
+							configurationStop, configurationMiddleStop - configuration, false);
 					solvers[i].start();
 				}
 			}
@@ -302,11 +365,13 @@ class NQueens {
 				if (i != numberOfCenterThreads - 1) {
 					solvers[numberOfLeftThreads + i] = new NQueensSolver((numberOfLeftThreads + i + 1), N,
 							configurationMiddle + (configurationsPerCenterThread * i),
-							configurationMiddle + (configurationsPerCenterThread * (i + 1)), true);
+							configurationMiddle + (configurationsPerCenterThread * (i + 1)),
+							configurationMiddleStop - configuration, true);
 					solvers[numberOfLeftThreads + i].start();
 				} else {
 					solvers[numberOfLeftThreads + i] = new NQueensSolver((numberOfLeftThreads + i + 1), N,
-							configurationMiddle + (configurationsPerCenterThread * i), configurationMiddleStop, true);
+							configurationMiddle + (configurationsPerCenterThread * i), configurationMiddleStop,
+							configurationMiddleStop - configuration, true);
 					solvers[numberOfLeftThreads + i].start();
 				}
 			}
@@ -315,17 +380,18 @@ class NQueens {
 				try {
 					solvers[i].t.join();
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
 
-		ArrayList<Integer> keys = Collections.list(NQueensSolver.assignments.keys());
-		Collections.sort(keys);
-		for (int i = 0; i < keys.size(); ++i) {
-			System.out.println(keys.get(i) + " " + NQueensSolver.assignments.get(keys.get(i)));
-		}
+		/*
+		 * ArrayList<Integer> keys =
+		 * Collections.list(NQueensSolver.assignments.keys());
+		 * Collections.sort(keys); for (int i = 0; i < keys.size(); ++i) {
+		 * System.out.println(keys.get(i) + " " +
+		 * NQueensSolver.assignments.get(keys.get(i))); }
+		 */
 
 		System.out.println("Number of solutions - "
 				+ ((NQueensSolver.solutions.size() * 2) + NQueensSolver.middleSolutions.size()));
@@ -347,7 +413,7 @@ class NQueens {
 
 			System.out.println();
 		}
-		
+
 		boardKeys = Collections.list(NQueensSolver.middleSolutions.keys());
 
 		Collections.sort(boardKeys);
@@ -363,7 +429,7 @@ class NQueens {
 
 			System.out.println();
 		}
-		
+
 		boardKeys = Collections.list(NQueensSolver.solutions.keys());
 
 		Collections.sort(boardKeys);
@@ -373,8 +439,10 @@ class NQueens {
 			int[][] board = NQueensSolver.solutions.get(boardKeys.get(i));
 
 			for (int j = 0; j < board.length; ++j) {
-				String[] reversedArray = new StringBuffer(Arrays.toString(board[j]).substring(1, Arrays.toString(board[j]).length() - 1)).reverse().toString().split(" ,");
-				
+				String[] reversedArray = new StringBuffer(
+						Arrays.toString(board[j]).substring(1, Arrays.toString(board[j]).length() - 1)).reverse()
+								.toString().split(" ,");
+
 				System.out.println("[" + String.join(", ", reversedArray) + "]");
 			}
 
